e316e6f2ab468f23aefb19c038021f6fd7d342f9
CAROSINE <moshiur287rahman@gmail.com>
2026-02-07 04:01:17 +0600

    codex

--- QR_codes/generate.py
+++ QR_codes/generate.py
@@ -1,4 +1,12 @@
 import qrcode
 
 url = input("Enter the URl to generate QR code: ").strip() #strip for unnecessary spaces
-file_path = "qrcode.png" #file path to save the QR code image
+file_path = "E:\\BitRox\\Python\\QR_codes\\qrcode.png" #file path to save the QR code image
+
+qr = qrcode.QRCode()
+qr.add_data(url)
+
+img = qr.make_image()
+img.save(file_path)
+
+print("QR code was generated and saved at:", file_path)
