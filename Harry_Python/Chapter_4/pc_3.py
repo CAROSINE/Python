78b9e4e68252ccda4862f92337621da1dea4c2af
CAROSINE <moshiur287rahman@gmail.com>
2026-02-10 19:15:57 +0600

    _

---
+++ Harry_Python/Chapter_4/pc_3.py
@@ -0,0 +1,3 @@
+a = (28,408, "Sajol") # tuple is immutable 
+
+a[2] = "Fajol" # this will give an error because we cannot change the value of a tuple once it is created.
---
+++ Harry_Python/Chapter_4/pc_4.py
@@ -0,0 +1,3 @@
+l = [301,305,408]
+
+print(sum(l)) # sum() function ta list er element gulo ke add kore total return kore    
