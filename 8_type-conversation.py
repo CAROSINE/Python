31946efe431676e630c95e049b4a24b4056eae41
CAROSINE <moshiur287rahman@gmail.com>
2026-02-12 12:47:08 +0600

    conversion

---
+++ 8_type-conversation.py
@@ -0,0 +1,14 @@
+import math
+
+
+print(math.sqrt(25)) # Output: 5.0
+print(math.factorial(5)) # Output: 120
+print(math.fabs(-5.5)) # Output: 5.5
+print(math.sin(math.radians(45))) # Output: 0.7071067811865475
+print(math.cos(math.radians(60))) # Output: 0.5
+print(math.tan(math.radians(30))) # Output: 0.5773502691896257 
+
+print(math.log(10)) # Output: 2.302585092994046
+print(math.log10(10)) # Output: 1.0
+
+print(math.gcd(48, 18)) # Output: 6
