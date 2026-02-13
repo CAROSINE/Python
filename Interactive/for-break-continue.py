f711cdbb1b4b1818bf96b6fffa8e129939173623
CAROSINE <moshiur287rahman@gmail.com>
2026-02-13 09:34:45 +0600

    break and continue

---
+++ Interactive/for-break-continue.py
@@ -0,0 +1,8 @@
+namelist = ['Sajib','Jeni','Sumon','Jahid','Mahin']
+
+for name in namelist:
+    if name == "Sumon":
+        continue
+    if name == "Jahid":
+        break
+    print(name + " is a talented person")
