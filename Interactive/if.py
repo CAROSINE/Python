3da563fcdeb33cbbc860819ecf05ad89dc92e388
CAROSINE <moshiur287rahman@gmail.com>
2026-02-13 09:04:42 +0600

    condition

--- Interactive/if.py
+++ Interactive/if.py
@@ -3,4 +3,4 @@ if marks >=80:
     print("A+")
 
 else:
-    print("A")
+    print("A") # condition is like ladder, if condition is true then it will execute the block of code under if statement otherwise it will execute the block of code under else statement.
