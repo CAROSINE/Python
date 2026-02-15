558e07aa3f56abde6b726dde15a46afaf4902242
CAROSINE <moshiur287rahman@gmail.com>
2026-02-16 02:47:04 +0600

    remove from  last item

--- Interactive/pop.py
+++ Interactive/pop.py
@@ -1,4 +1,4 @@
 cities = ["Dhaka", "Chittagong", "Khulna", "Barishal"]
 
 cities.pop(2) # (2) is the index of the element to be removed
-print(cities)
+print(cities) # we can also use several pop() to remove several elements from the list.
