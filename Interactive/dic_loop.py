6ed0ae2ce457ffa64cb726a3d4a3d87d64784be6
CAROSINE <moshiur287rahman@gmail.com>
2026-02-15 07:54:53 +0600

    loop

---
+++ Interactive/dic_loop.py
@@ -0,0 +1,8 @@
+person = {                         # person is a dictionary
+    "Name": "Sajol",               # key: Name, value: Sajol
+    "Age": 21,                     # value is an integer/string
+    "City": "Dhaka"
+}
+
+for KEY, VALUE in person.items():
+    print(f"Key: {KEY} value: {VALUE}")
