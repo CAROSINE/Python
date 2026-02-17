5bfac89f4200188d919704c223fccb8dd2ced95d
CAROSINE <moshiur287rahman@gmail.com>
2026-02-17 09:50:08 +0600

    reverse

--- Interactive/reverse.py
+++ Interactive/reverse.py
@@ -2,3 +2,5 @@ cities = ["Dhaka", "Chittagong", "Khulna", "Barishal"]
 
 cities.reverse() # reverse() is used to reverse the order of the elements in the list.
 print(cities) # we can also use several pop() to remove several elements from the list.
+length = len(cities)
+print(length) # len() is used to find the length of the list. It returns the number of elements in the list.
