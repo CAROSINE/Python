fca4fc34977ad986f0a1363d1edb4309ca66a3b5
CAROSINE <moshiur287rahman@gmail.com>
2026-04-09 02:17:05 +0600

    union/intersection

---
+++ Harry_Python/Chapter_5/04_set-u_n.py
@@ -0,0 +1,7 @@
+s1={1,2,3}
+s2={3,4,5,5}
+
+print(s1.union(s2)) #print(s1 | s2) # for union
+print(s1.intersection(s2)) #print(s1 & s2) # for intersection
+print(s1.difference(s2)) #print(s1 - s2) # for difference
+print(s1.symmetric_difference(s2)) #print(s1 ^ s2) # for symmetric difference
