bdc90a34538ff2c7571703767198abce0236385d
CAROSINE <moshiur287rahman@gmail.com>
2026-04-09 04:23:23 +0600

    set

---
+++ Harry_Python/Chapter_5/pc_01.py
@@ -0,0 +1,9 @@
+words = {
+        "sahajjo" : "help",
+        "bhalo" : "good",
+        "kharap" : "bad",
+        "kotha" : "word"
+        }
+
+word = input("Enter the word you want meaning of: ")
+print(words[word])
