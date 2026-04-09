57b4f4fbc1456ad35b70e7003ad9e9a877aea80f
CAROSINE <moshiur287rahman@gmail.com>
2026-04-09 10:40:07 +0600

    ^_^

---
+++ Harry_Python/Chapter_5/pc_05.py
@@ -0,0 +1,18 @@
+f = {}
+
+name = input("Enter friends name: ")
+lang = input("Enter language name: ")
+
+f.update({name: lang})
+name = input("Enter friends name: ")
+lang = input("Enter language name: ")
+
+f.update({name: lang})
+name = input("Enter friends name: ")
+lang = input("Enter language name: ")
+
+f.update({name: lang})
+name = input("Enter friends name: ")
+lang = input("Enter language name: ")
+
+print(f)
