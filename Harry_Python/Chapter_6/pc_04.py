8b4b2f35e1b5063795bb90e7c2164280f6b12b40
CAROSINE <moshiur287rahman@gmail.com>
2026-04-10 02:00:45 +0600

    len in elif

---
+++ Harry_Python/Chapter_6/pc_04.py
@@ -0,0 +1,6 @@
+username = input("Enter your name: ")
+
+if(len(username)<10):
+    print("Your name is less than 10 characters,")
+else:
+    print("Your name is equal or not less than 10 characters.")
