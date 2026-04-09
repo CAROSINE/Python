03abe4e9a1d55b5dbe85e4a6520342d0b3bf41cd
CAROSINE <moshiur287rahman@gmail.com>
2026-04-10 02:11:47 +0600

    list in elif

---
+++ Harry_Python/Chapter_6/pc_05.py
@@ -0,0 +1,9 @@
+l = ["Mahin" , "Mahi" , "Olivia" , "Carosine"]
+
+name = input("Enter your name: ")
+
+if(name in l):
+    print("Your name in the list.")
+
+else:
+    print("Your name is not in  the list.")
