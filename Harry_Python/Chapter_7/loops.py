82a60e34f177b0a3cfc7c61d8e9fae035cf8e207
CAROSINE <moshiur287rahman@gmail.com>
2026-04-10 12:52:27 +0600

    for loop

---
+++ Harry_Python/Chapter_7/loops.py
@@ -0,0 +1,10 @@
+#print(1)
+#print(2)
+#print(3)
+#print(4)
+#print(5)  # output  will be 1,2,3,4,5
+
+# The same  task can don elike this 
+
+for i in range (1,6):    # This is how for loop done
+    print(i)            # Start from 1 and end at 5 because 6 is not included in the range function
