c758c3eaeee902c545a583ed593200dde76e8c56
CAROSINE <moshiur287rahman@gmail.com>
2026-04-11 20:15:42 +0600

    while loop

---
+++ Harry_Python/Chapter_7/list-using_while.py
@@ -0,0 +1,7 @@
+l = [1 , "mahin" , True , "This" , "Ramim" , "Raim"]
+
+i = 0
+
+while(i<len(l)):
+    print(l[i]) # This is how we can print the list using while loop
+    i += 1 # This is how we can increase the value of i by 1 in while loop 
