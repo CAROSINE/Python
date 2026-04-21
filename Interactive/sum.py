13836aca7265fa15c0cd1789e88249b8ad6ef588
CAROSINE <moshiur287rahman@gmail.com>
2026-04-21 11:24:41 +0600

    method/object/class

---
+++ Interactive/sum.py
@@ -0,0 +1,12 @@
+class summation:
+    #variable
+    x=10
+    y=20
+
+    #method
+    def add(self):
+        sum = self.x+self.y
+        print(sum)
+
+obj = summation()   # object create
+obj.add()           # method call 
