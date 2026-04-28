4aeeeac045a83c11be4519dd3f57921c6ffce79c
CAROSINE <moshiur287rahman@gmail.com>
2026-04-28 23:15:53 +0600

    .

---
+++ calculator.py
@@ -0,0 +1,18 @@
+# Simple Calculator using OOP
+
+class Calculator:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def add(self):
+        return self.a + self.b
+
+    def subtract(self):
+        return self.a - self.b
+
+# object create
+calc = Calculator(10, 5)
+
+print("Addition:", calc.add())
+print("Subtraction:", calc.subtract())
