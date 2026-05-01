77cc03af9c5392b75987f994c1562aebd3d884f9
CAROSINE <moshiur287rahman@gmail.com>
2026-05-01 17:54:15 +0600

    ^_^

--- chess board cc.py
+++ chess board cc.py
@@ -1,14 +1,14 @@
 import numpy as np 
 import matplotlib.pyplot as plt
 
-def plot_chessboard(size=8):
+def plot_chessboard( size = 8 ):
     # Generate checkerboard pattern
-    board = np.indices((size, size)).sum(axis=0) % 2
+    board = np.indices((size, size)).sum( axis=0 ) % 2
 
     # Define chessboard colors
     cmap = plt.cm.colors.ListedColormap(["#f0d9b5", "#b58863"])
 
-    fig, ax = plt.subplots(figsize=(6, 6))
+    fig, ax = plt.subplots(figsize = (6, 6))
     ax.imshow(board, cmap=cmap, interpolation="nearest")
 
     # Set ticks and labels
