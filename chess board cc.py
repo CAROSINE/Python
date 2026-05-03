0d5328aebcc38aed30cc4f3ae206d8adce70cd1b
CAROSINE <moshiur287rahman@gmail.com>
2026-05-03 19:41:07 +0600

    ^_^

--- chess board cc.py
+++ chess board cc.py
@@ -9,7 +9,7 @@ def plot_chessboard( size = 8 ):
     cmap = plt.cm.colors.ListedColormap(["#f0d9b5", "#b58863"])
 
     fig, ax = plt.subplots(figsize = (6, 6))
-    ax.imshow(board, cmap=cmap, interpolation="nearest")
+    ax.imshow(board, cmap = cmap, interpolation = "nearest")
 
     # Set ticks and labels
     ax.set_xticks(np.arange(size))
