8026633f5bbb2c58a195f4d63cf58d54f79d2d38
Md.Moshiur Rahman Sajol <moshiur287rahman@gmail.com>
2026-05-04 23:16:18 +0600

    Implement Car class for speed control

---
+++ car_speed.py
@@ -0,0 +1,26 @@
+# Car speed control using class
+
+class Car:
+    def __init__(self, brand):
+        self.brand = brand
+        self.speed = 0
+
+    def accelerate(self):
+        self.speed += 10
+        print("Speed increased to:", self.speed)
+
+    def brake(self):
+        if self.speed >= 10:
+            self.speed -= 10
+        print("Speed decreased to:", self.speed)
+
+    def show(self):
+        print("Car:", self.brand, "| Speed:", self.speed)
+
+
+# object create
+c1 = Car("Toyota")
+c1.accelerate()
+c1.accelerate()
+c1.brake()
+c1.show()
