f2256a280f2e28d79021030a2073ff9a7b612ee3
CAROSINE <moshiur287rahman@gmail.com>
2026-05-09 23:54:01 +0600

    open-pc

---
+++ Harry_Python/Chapter_9/Practice_set/pc_01.py
@@ -0,0 +1,9 @@
+f = open("test.txt") 
+content = f.read()
+if("win" in content):
+    print("Yes, win is present in the file ")
+
+else:
+    print("No, win is not present in the file ")
+
+f.close()
