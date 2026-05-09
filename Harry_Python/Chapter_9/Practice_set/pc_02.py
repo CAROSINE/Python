2e9364ebab299a96344d912e5220f535f21a0e6f
CAROSINE <moshiur287rahman@gmail.com>
2026-05-09 23:59:42 +0600

    game

---
+++ Harry_Python/Chapter_9/Practice_set/pc_02.py
@@ -0,0 +1,14 @@
+import random
+
+def game():
+    rand_num = random.randint(1, 100)
+    user_input = None
+
+    while user_input != rand_num:
+        user_input = int(input("Guess the number between 1 and 100: "))
+        if user_input < rand_num:
+            print("Too low! Try again.")
+        elif user_input > rand_num:
+            print("Too high! Try again.")
+        else:
+            print("Congratulations! You've guessed the number.")
