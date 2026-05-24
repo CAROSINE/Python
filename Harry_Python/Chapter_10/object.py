e516a6e88691ec854834d6b22d5a04af1941088a
CAROSINE <moshiur287rahman@gmail.com>
2026-05-24 13:33:15 +0600

    create object

--- Harry_Python/Chapter_10/object.py
+++ Harry_Python/Chapter_10/object.py
@@ -14,7 +14,3 @@ print(Saj.name, Saj.salary, Saj.language)
 
 # Here name is object attributr and salary and language are
 #  class attribute as they directly belong to the class
-
-
-
-# create object 
