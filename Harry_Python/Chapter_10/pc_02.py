f43e033826a1c84891dc59c600ae561ab4a8c4b9
CAROSINE <moshiur287rahman@gmail.com>
2026-05-28 23:44:11 +0600

    Calculator creating Class

--- Harry_Python/Chapter_10/pc_02.py
+++ Harry_Python/Chapter_10/pc_02.py
@@ -2,10 +2,10 @@ class Calculator:
 
     def __init__(self, n):
         self.n = n
-
+  
     def square(self):
         print(f"The square is {self.n * self.n}")
- 
+      
                  # or  
 a = Calculator   # a = Calculator(5)
 a(5).square()    # a.square()
