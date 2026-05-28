1218f0aa875b90190d79e92b5d9212de285d4584
Md.Moshiur Rahman Sajol <moshiur287rahman@gmail.com>
2026-05-28 23:38:21 +0600

    Add whitespace for better code readability

--- car_speed.py
+++ car_speed.py
@@ -4,15 +4,18 @@ class Car:
     def __init__(self, brand):
         self.brand = brand
         self.speed = 0
+        
 
     def accelerate(self):
         self.speed += 10
         print("Speed increased to:", self.speed)
+        
 
     def brake(self):
         if self.speed >= 10:
             self.speed -= 10
         print("Speed decreased to:", self.speed)
+        
 
     def show(self):
         print("Car:", self.brand, "| Speed:", self.speed)
