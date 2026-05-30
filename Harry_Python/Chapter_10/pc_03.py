fdc16ccb90cad451176edb70497d26e9890c0335
CAROSINE <moshiur287rahman@gmail.com>
2026-05-30 23:42:21 +0600

    attribute

---
+++ Harry_Python/Chapter_10/pc_03.py
@@ -0,0 +1,12 @@
+class Demo:
+    a = 4
+
+o = Demo()  # object
+print(o.a)
+o.a = 0   # condiotion from question 
+
+print(o.a)
+
+# prints the class attribute because instance attribute is not present in the object   5
+# instance attribute is created in the object and assigned value 0    6
+# prints the instance attribute because it is present in the object    7
