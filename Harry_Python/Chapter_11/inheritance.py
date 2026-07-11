743821455f2576d786f15e31903a6a0edaf66248
CAROSINE <moshiur287rahman@gmail.com>
2026-07-11 13:00:14 +0600

    inheritance

--- Harry_Python/Chapter_11/inheritance.py
+++ Harry_Python/Chapter_11/inheritance.py
@@ -1,10 +1,12 @@
 class Employee:
     company = "Google"
+    
     def show(self):
         print(f"Name: {self.name}, Age: {self.age}, Salary: {self.salary}")
 
 class Programmer:
     company = "Microsoft"
+
     def show(self):
         print(f"The name is {self.name} and the salary is {self.salary}")
 
