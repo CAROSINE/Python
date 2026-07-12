83f09e9be96df1cf20613c4cc6533ce56c739b78
CAROSINE <moshiur287rahman@gmail.com>
2026-07-13 00:00:28 +0600

    multilevelk  inheritance

---
+++ Harry_Python/Chapter_11/multilevel-inheritance.py
@@ -0,0 +1,12 @@
+from Harry_Python.Chapter_11.inheritance import Programmer
+
+
+class Employee:
+    a = 1
+
+class programmer(Employee):
+    b = 2
+
+class Managaer(Programmer):
+    c = 3
+     
