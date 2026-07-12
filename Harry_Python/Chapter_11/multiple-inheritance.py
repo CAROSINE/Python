92382af216d873d8fff1c2a2fb237f4593ffa61b
CAROSINE <moshiur287rahman@gmail.com>
2026-07-12 21:44:06 +0600

    multiple inheritance

---
+++ Harry_Python/Chapter_11/multiple-inheritance.py
@@ -0,0 +1,22 @@
+class Employee:
+    company = "Google"
+    
+    def show(self):
+        print(f"Name: {self.name}, Age: {self.age}, Salary: {self.salary}")
+
+
+
+
+
+class Programmer:
+    company = "Microsoft"
+
+    def show(self):
+        print(f"The name is {self.name} and the salary is {self.salary}")
+
+    def showLang(self):
+        print(f"The programming language is {self.language}")
+
+a = Employee()
+b = Programmer()
+print(a.company, b.company)
