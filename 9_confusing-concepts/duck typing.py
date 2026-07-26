834969e5646ffddc7bc4c8f260647c272ca517af
CAROSINE <moshiur287rahman@gmail.com>
2026-07-26 23:36:49 +0600

    py

--- 9_confusing-concepts/duck typing.py
+++ 9_confusing-concepts/duck typing.py
@@ -2,11 +2,14 @@ class DUCK:
     def quack(self):
        print("Quack!")
 
+
 class Dog:
     def quack(self):
        print("I am a Dog . I can quack")
 
+
 def make_it_quack(duck):
     duck.quack()
+
 
 make_it_quack
