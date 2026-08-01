852639288f4ca85361e4e9626b99706ff88d9f37
CAROSINE <moshiur287rahman@gmail.com>
2026-08-01 12:45:53 +0600

    PyTorch

---
+++ PyTorch/list-tensor.py
@@ -0,0 +1,20 @@
+import torch
+
+# Tensor creation
+x = torch.tensor([1, 2,3])
+y = torch.tensor([4, 5, 6])
+
+print("x:", x)
+print("y:", y)
+
+# Basic operations
+z = x + y
+print("x + y= ", z)
+
+#Matrix/tensor shape
+a = torch.rand(2, 3) # random 2X3 tensor
+print(a)
+print("Shape: ", a.shape)
+
+# GPU check
+print("CUDA available?", torch.cuda.is_available()) 
